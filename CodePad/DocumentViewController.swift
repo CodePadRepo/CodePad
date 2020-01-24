a1908b8a680930f0102197b1c7f1cd5c6bb2b558
Ryang Sohn <loop.infinitely@gmail.com>
2020-01-24 16:44:29 +0900

    Add file opening

--- CodePad/DocumentViewController.swift
+++ CodePad/DocumentViewController.swift
@@ -19,12 +19,27 @@ class DocumentViewController: UIViewController, WKUIDelegate {
         editorView.loadFileURL(url, allowingReadAccessTo: url)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
         document?.open(completionHandler: { (success) in
             if success {
+                do {
+                    self.editorView.evaluateJavaScript(
+                        "document.body.querySelector('#editor').innerText = `\(try String(contentsOf: self.document!.fileURL))`"
+                    ) { (result, error) in
+                        // TODO: Add proper error handling
+                        if error != nil {
+                            print(error!)
+                        } else {
+                            print(result!)
+                        }
+                    }
+                } catch {
+                    print("Failed to open file")
+                }
             } else {
+                // TODO: Add proper error handling
             }
         })
     }
