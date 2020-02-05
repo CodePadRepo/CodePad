fde659ccb8057b5eb6386831a98dc21f0c3b70b4
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-05 23:21:41 +0900

    Add URLHelper

---
+++ CodePad/URLHelper.swift
@@ -0,0 +1,21 @@
+//
+//  PathHelper.swift
+//  CodePad
+//
+//  Created by Ryang Sohn on 2020/02/05.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    static func cacheDir() -> URL {
+        guard let cacheURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            fatalError("Unable to get system cache directory")
+        }
+        #if targetEnvironment(simulator)
+        print("Accessed cache URL: \(cacheURL)")
+        #endif
+        return cacheURL
+    }
+}
