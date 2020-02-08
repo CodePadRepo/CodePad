db6ef5c1c0a06dc9658efff528ae2cc7c1bd5f29
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-08 17:58:15 +0900

    Change initializeEditor to set editor content

--- CodePad/Web Resources/TextEditor.js
+++ CodePad/Web Resources/TextEditor.js
@@ -1,14 +1,20 @@
 modelist = ace.require("ace/ext/modelist");
 
-initializeEditor = (theme, filename) => {
+initializeEditor = (theme, filename, editorContent) => {
     editor = ace.edit("editor");
     editor.setTheme(`ace/theme/${theme}`);
     const mode = modelist.getModeForPath(filename).mode
     editor.session.setMode(mode);
+    editor.setValue(editorContent)
+    editor.getSession().setUndoManager(new ace.UndoManager())
+    editor.clearSelection();
     
-    window.webkit.messageHandlers.editorMessageHandler.postMessage({
-        event: "editor_ready",
-        data: []
+    editor.session.on("change", () => {
+        console.log("Text changed");
+        window.webkit.messageHandlers.editorMessageHandler.postMessage({
+            event: "text_change",
+            data: [editor.getValue()]
+        });
     });
 };
 
