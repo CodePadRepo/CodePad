facc7c5c82ae06f9cef770b4b604a358fc363791
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-08 20:35:12 +0900

    Add file creation

--- CodePad/View Controllers/DocumentBrowserViewController.swift
+++ CodePad/View Controllers/DocumentBrowserViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 
 class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocumentBrowserViewControllerDelegate {
+    static let newDocNumberKey = "newDocNumber"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +21,52 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
         allowsPickingMultipleItems = false
     }
     
+    fileprivate func generateDocumentName() -> String {
+        let newDocNumber = UserDefaults.standard.integer(forKey: DocumentBrowserViewController.newDocNumberKey)
+        return "Untitled Code \(newDocNumber)"
+    }
+    
+    fileprivate func incrementNameCount() {
+        let newDocNumber = UserDefaults.standard.integer(forKey: DocumentBrowserViewController.newDocNumberKey) + 1
+        UserDefaults.standard.set(newDocNumber, forKey: DocumentBrowserViewController.newDocNumberKey)
+    }
+    
+    fileprivate func createDocumentURL(_ documentName: String) -> URL {
+        let stubURL = UIApplication.cacheURL().appendingPathComponent(documentName)
+        return stubURL
+    }
+    
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
-        let newDocumentURL: URL? = nil
+        let defaultDocumentName = generateDocumentName()
+        var documentName: String? = nil
+        incrementNameCount()
         
-        if newDocumentURL != nil {
-            importHandler(newDocumentURL, .move)
-        } else {
-            importHandler(nil, .none)
-        }
+        let alert = UIAlertController(title: "Create File", message: "Enter file name", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.text = defaultDocumentName
+            textField.placeholder = "File name"
+        })
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            documentName = alert.textFields?.first?.text ?? defaultDocumentName
+            let newDocumentURL = self.createDocumentURL(documentName!)
+            let newDocument = CodePadDocument(fileURL: newDocumentURL)
+            
+            newDocument.save(to: newDocumentURL, for: .forCreating) { success in
+                guard success else {
+                    importHandler(nil, .none)
+                    return
+                }
+                newDocument.close { (closeSuccess) in
+                    guard closeSuccess else {
+                        importHandler(nil, .none)
+                        return
+                    }
+                    importHandler(newDocumentURL, .move)
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
