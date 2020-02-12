eeb74c45ec60809b9c4fbbe4a2834cc22a89ce7d
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-12 21:56:33 +0900

    Rename CodePadUITests.swift

---
+++ CodePadUITests/DocumentBrowserViewControllerTests.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentBrowserViewControllerTests.swift
+//  CodePadUITests
+//
+//  Created by Ryang Sohn on 2020/02/10.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import XCTest
+
+class DocumentBrowserViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
