471a2e7ce38d3fd461c6bf91e04cb2fce2b7d69c
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-13 22:59:51 +0900

    Rename UI tests file... again
    What a stupid decision...

---
+++ CodePadUITests/CodePadUITests.swift
@@ -0,0 +1,26 @@
+//
+//  CodePadUITests.swift
+//  CodePadUITests
+//
+//  Created by Ryang Sohn on 2020/02/10.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import XCTest
+
+class CodePadUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testNavbar() {
+        
+    }
+
+}
