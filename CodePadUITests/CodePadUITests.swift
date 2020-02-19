05fa1d20b4eff4a464efdada853fa0ad9de58fc8
Ryang Sohn <loop.infinitely@gmail.com>
2020-02-19 23:15:31 +0900

    Revert "Add document delete test"
    This reverts commit a36da0942db38ea1446186d82ae183a4b1e5dd36.
    I thought the commit wouldn't break Travis CI build, but it actually did
    just like the last time when I did the same thing.

--- CodePadUITests/CodePadUITests.swift
+++ CodePadUITests/CodePadUITests.swift
@@ -44,18 +44,6 @@ class CodePadUITests: XCTestCase {
         XCTAssertEqual(filenameTextfield.placeholderValue, "File name")
         filenameTextfield.clearAndTypeText(newFileName)
         app.alerts.element.buttons["OK"].tap()
-        
-        sleep(2)
-        let predicate = NSPredicate(format: "label CONTAINS '\(newFileName)'")
-        if app.navigationBars.staticTexts.containing(predicate).firstMatch.exists {
-            app.navigationBars.buttons.firstMatch.tap()
-        }
-        
-        sleep(2)
-        let newFileInBrowser = app.cells.containing(predicate).firstMatch
-        XCTAssert(newFileInBrowser.exists)
-        newFileInBrowser.press(forDuration: 2)
-        app.buttons["Delete"].tap()
     }
 
 }
