dcc737c3de5d4f592aba9202353483fd47453a9d
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-04 23:04:41 +0900

    Add config menu stub for keybindings

---
+++ CodePad/View Controllers/KeybindingConfigViewController.swift
@@ -0,0 +1,13 @@
+//
+//  KeybindingConfigViewController.swift
+//  CodePad
+//
+//  Created by Ryang Sohn on 2020/03/04.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import UIKit
+
+class KeybindingConfigViewController: UITableViewController {
+    
+}
